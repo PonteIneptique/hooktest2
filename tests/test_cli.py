8f5f5c70d5884109bfab144ece1df0dfc5cfdb09
Thibault Clérice <leponteineptique@gmail.com>
2025-07-17 16:53:07 +0200

    Adding more tests, including for double tree

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -12,21 +12,44 @@ def get_path(xml: str) -> str:
 def count_failing(result: Result) -> int:
     return len([s for s in result.statuses if s.status == False])
 
+
 @pytest.fixture
 def runner():
     return CliRunner()
 
+
+def test_simple_passing_file(runner):
+    """Test with a file expected to pass."""
+    result = runner.invoke(cli, ['--no-catalog', get_path("correct_simple.xml")], standalone_mode=False)
+    assert '✗' not in result.output, "File has a failing test"
+    assert 'duplicateRefs[Tree=default]' not in result.output, "Tree Default has no dup reff"
+    assert count_failing(result.return_value.results[get_path("duplicate.xml")]) == 0, "Zero failing test"
+
+
+def test_double_tree_file(runner):
+    """Test with a file expected to pass."""
+    result = runner.invoke(cli, ['--no-catalog', '-v', 'verbose', get_path("correct_double_tree.xml")], standalone_mode=False)
+    assert '✗' not in result.output, "File has a failing test"
+    assert "parse(citeStructures): Tree:default->line(15) " in result.output, "Both tree are documented"
+    assert "Tree:translations->language(3)->[line(12)]" in result.output, "Both tree are documented"
+    assert "forbiddenRefs[Tree=default]: ✔" in result.output, "Both tree are documented"
+    assert "duplicateRefs[Tree=default]: ✔" in result.output, "Both tree are documented"
+    assert "forbiddenRefs[Tree=translations]: ✔" in result.output, "Both tree are documented"
+    assert "duplicateRefs[Tree=translations]: ✔" in result.output, "Both tree are documented"
+    assert count_failing(result.return_value.results[get_path("correct_double_tree.xml")]) == 0, "Zero failing test"
+
+
 def test_duplicate_refs(runner):
     """Test whether duplicate ref finding is working"""
-
-    # Test with a file expected to fail.
     result = runner.invoke(cli, ['--no-catalog', get_path("duplicate.xml")], standalone_mode=False)
     assert '✗' in result.output, "File has a failing test"
     assert 'duplicateRefs[Tree=default]' in result.output, "Tree Default has duplicate reff"
     assert count_failing(result.return_value.results[get_path("duplicate.xml")]) == 1, "Only one failing test"
 
-    # Test with a file expected to pass.
-    result = runner.invoke(cli, ['--no-catalog', get_path("correct_simple.xml")], standalone_mode=False)
-    assert '✗' not in result.output, "File has a failing test"
-    assert 'duplicateRefs[Tree=default]' not in result.output, "Tree Default has no dup reff"
-    assert count_failing(result.return_value.results[get_path("duplicate.xml")]) == 0, "Zero failing test"
+
+def test_forbidden_ref(runner):
+    """Test with a file expected to fail on forbidden refs."""
+    result = runner.invoke(cli, ['--no-catalog', get_path("forbid.xml")], standalone_mode=False)
+    assert '✗' in result.output, "File has a failing test"
+    assert 'forbiddenRefs[Tree=default]' in result.output, "Tree Default has forbidden references"
+    assert count_failing(result.return_value.results[get_path("forbid.xml")]) == 1, "Only one failing test"
--- tests/test_file.py
+++
@@ -1,9 +0,0 @@
-from hooktest.tester import Tester
-import os.path
-
-op = lambda x: os.path.join(os.path.dirname(os.path.abspath(__file__)), x)
-
-p = Tester()
-p.ingest([op("./test_data/catalog.xml")])
-p.tests()
-print(p.results)
